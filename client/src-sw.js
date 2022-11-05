17db8ed7bb964fc272c155416b2befaf16ce2e4c
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-05 13:03:33 -0600

    deploy to heroku

---
+++ client/src-sw.js
@@ -0,0 +1,48 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+
+registerRoute(
+  ({ request }) => request.destination === "images",
+
+  // sets cache first strategy for images
+  new CacheFirst({
+    cacheName: "image-cache",
+    plugins: [
+      // plugin added for cacheable responses
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        // sets expiration for 30 days hence
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
---
+++ client/src/js/database.js
@@ -0,0 +1,52 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ jate: content });
+  const result = await request;
+  console.log('Data saved to the database', result)
+};
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  // console.log('result.value', result);
+  return result;
+};
+
+// DELETE method for indexedDB
+// export const deleteDb = async (id) => {
+//   console.log('DELETE from the database', id);
+//   const todosDb = await openDB('jate', 1);
+//   // tx needed to be changed to readwrite
+//   const tx = todosDb.transaction('jate', 'readwrite');
+//   const store = tx.objectStore('jate');
+//   const request = store.delete(id);
+//   const result = await request;
+//   console.log('result.value', result);
+//   return result;
+// };
+
+initdb();
