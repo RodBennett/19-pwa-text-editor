a2dd0b69a1d34ab3291d64de7cec80ccd5ccc641
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-05 12:48:52 -0600

    all functionality, but not renedering as expected upon reload

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
+  // console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -31,8 +31,21 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
+  // console.log('result.value', result);
   return result;
-}
+};
+
+// DELETE method for indexedDB
+// export const deleteDb = async (id) => {
+//   console.log('DELETE from the database', id);
+//   const todosDb = await openDB('jate', 1);
+//   // tx needed to be changed to readwrite
+//   const tx = todosDb.transaction('jate', 'readwrite');
+//   const store = tx.objectStore('jate');
+//   const request = store.delete(id);
+//   const result = await request;
+//   console.log('result.value', result);
+//   return result;
+// };
 
 initdb();
--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -25,8 +25,13 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.info('Loaded data from IndexedDB, injecting into editor', header, data);
+      // this.editor.setValue(data[0].jate || localData || header)
+      if (data.length !== 0) {
+        this.editor.setValue(data[0].jate || localData || header)
+       } else {
+        this.editor.setValue(header)
+      }
     });
 
     this.editor.on('change', () => {
